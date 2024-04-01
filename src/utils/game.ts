fe8dac605039a5cab5a46811abec11091e8f1089
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-04-01 12:35:07 -0300

    add game functions

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,7 +1,49 @@
+import { words } from "../data/words";
 
+export const matriz = [
+  ["0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0"],
+];
 
-export function restartGame(matriz: Array<Array<string>>) {
+export const game = {
+  activeArray: 0,
+  word: getRandomWord(words),
+}
+
+export function getRandomWord(lista: Array<string>) {
+  let rnd = Math.random();
+  let item = Math.floor(rnd * lista.length);
+  return lista[item].toUpperCase();
+}
+
+export function restartGame(/* matriz: Array<Array<string>> */) {
   matriz.forEach(array => {
-    array.forEach(el => el = '0')
+    array.fill('0')
+  });
+
+  game.word = getRandomWord(words)
+  game.activeArray = 0;
+  restoreCells();
+  restoreButtons();
+}
+
+function restoreCells (){
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.classList.remove('bg-green-400','bg-grayt-400','bg-yellow-400');
+    cell.classList.add('bg-white');
+    cell.textContent = '';
+  })
+}
+
+function restoreButtons(){
+  const buttons = document.querySelectorAll('.qwerty');
+  buttons.forEach(btn => {
+    btn.classList.remove('bg-green-400','bg-grayt-400','bg-yellow-400');
+    btn.classList.add('bg-white');
   })
 }
