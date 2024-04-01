226b26c390fba136be636d9ffe39b67ea0bd4448
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-04-01 16:57:22 -0300

    adding other words

--- src/data/words.ts
+++ src/data/words.ts
@@ -75,4 +75,15 @@ export const words = [
   "campo",
   "ficha",
   "facha",
+  "fuera",
+  "fiera",
+  "banco",
+  "manco",
+  "stich",
+  "sofia",
+  "elias",
+  "barco",
+  "largo",
+  "peque",
+  
 ]
